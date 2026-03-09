c5710be0c4c008caf806fe7e1c8b0442ee36296b
Joshua Mathew <joshm333@gmail.com>
2026-03-09 15:11:57 -0400

    Add files via upload

--- fittrack/app/build.gradle.kts
+++ fittrack/app/build.gradle.kts
@@ -65,4 +65,6 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     implementation(libs.hilt.navigation.compose)
+
+    implementation(libs.coil.compose)
 }
